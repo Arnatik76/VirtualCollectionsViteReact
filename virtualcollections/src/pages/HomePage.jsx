07a31ec03841eae2a73a304d1b24bbd87f819aa9
Arnatik76 <118046506+Arnatik76@users.noreply.github.com>
2025-06-01 23:48:50 +0500

    Implement routing with HomePage component

---
+++ virtualcollections/src/pages/HomePage.jsx
@@ -0,0 +1,11 @@
+function HomePage() {
+    return (
+        <div className="flex">
+            <p className="text-2xl">
+                This is Home Page
+            </p>
+        </div>
+    )
+}
+
+export default HomePage;
